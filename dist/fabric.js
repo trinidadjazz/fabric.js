26f616fc85baf4c32b17f99bf6a353e572220730
javascripFan <wdeveloping9099@gmail.com>
2019-04-10 13:02:10 -0700

    solved 1 2 requirements

--- dist/fabric.js
+++ dist/fabric.js
@@ -28008,7 +28008,6 @@ fabric.util.object.extend(fabric.IText.prototype, /** @lends fabric.IText.protot
         widthBeforeCursor = this._getWidthBeforeCursor(lineIndex, charIndex),
         indexOnOtherLine = this._getIndexOnLine(lineIndex + 1, widthBeforeCursor),
         textAfterCursor = this._textLines[lineIndex].slice(charIndex);
-    console.log(textAfterCursor.length + indexOnOtherLine + 2);
     return textAfterCursor.length + indexOnOtherLine + 2;
   },
 
